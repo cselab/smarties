72a31d0fea1ab89e7002cb240b7b9a86b2d90a29
Guido Novati <novatig@gmail.com>
2018-07-17 14:31:34 +0300

    improved network plot

--- pytools/plot_weights.py
+++ pytools/plot_weights.py
@@ -18,6 +18,7 @@ FILE=    sys.argv[1]
 W  = np.fromfile(FILE+"weights.raw", dtype=np.float64)
 M1 = np.fromfile(FILE +"1stMom.raw", dtype=np.float64)
 M2 = np.fromfile(FILE +"2ndMom.raw", dtype=np.float64)
+TGT= np.fromfile(FILE +"tgt_weights.raw", dtype=np.float64)
 INDS = np.where(M2>1e-16)
 #INDS = np.arange(64768,73984)
 #INDS = np.reshape(INDS, [128,72])
@@ -28,12 +29,13 @@ INDS = np.where(M2>1e-16)
 W  = W[INDS];
 M1 = M1[INDS];
 M2 = M2[INDS];
+TGT=TGT[INDS];
 assert(not np.isnan(W).any())
 assert(not np.isnan(M1).any())
 assert(not np.isnan(M2).any())
 
 plt.subplot(231)
-plt.semilogy(1e-6 + abs(W),'b.')
+plt.semilogy(1e-12 + abs(W),'b.')
 plt.title('Weights')
 #plt.plot(abs(W),'b.')
 
@@ -42,7 +44,7 @@ plt.semilogy(abs(M1) + 1e-12,'k.')
 plt.title('Abs M1')
 
 plt.subplot(233)
-plt.semilogy(1e-6 + np.sqrt(M2), 'g.')
+plt.semilogy(1e-12 + np.sqrt(M2), 'g.')
 #plt.semilogy(abs(M)/S,'g.')
 plt.title('Sqrt M2')
 
@@ -51,11 +53,15 @@ plt.semilogy(abs(M1)/(1e-7 + np.sqrt(M2)) + 1e-6,'g.')
 plt.title('Abs Grad')
 
 plt.subplot(235)
-plt.semilogy((abs(W)*2.2e-16)/(np.sqrt(M2)),'k.')
+if len(sys.argv) > 2: LAMBDA=float(sys.argv[2])
+else: LAMBDA = 2.2e-16
+plt.semilogy( abs(W)*LAMBDA/(1e-7 + np.sqrt(M2)),'k.')
+plt.title('Penal. grad')
 
 
 plt.subplot(236)
 #plt.semilogy(abs(W)*1.19209e-07 + 1e-12,'r.',alpha=0.2)
-plt.semilogy((abs(W)*2.2e-16)/(abs(M1)),'k.')
+plt.semilogy(abs(W-TGT),'k.')
+plt.title('Shift from TGT')
 
 plt.show()
