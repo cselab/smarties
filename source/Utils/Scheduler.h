0c9963ab487b11b19b7f1024a9956ecc4eabd3b9
Guido Novati <16716298+novatig@users.noreply.github.com>
2018-09-14 19:05:34 +0200

    Bugfix

--- source/Utils/Scheduler.h
+++ source/Utils/Scheduler.h
@@ -161,8 +161,8 @@ private:
     } else {
       lock_guard<mutex> lock(mpi_mutex);
       MPI_Isend(outBufs[i-1],outSize, MPI_BYTE, i,0,workersComm,&tmp);
-      MPI_Request_free(&tmp); //Not my problem
     }
+    MPI_Request_free(&tmp); //Not my problem
   }
 
   inline void recvBuffer(const int i)
